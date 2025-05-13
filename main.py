e65ca2fcdef9aa2607724ba1d53c7cadba1c9fa8
swayam <swayambansal01@gmail.com>
2025-05-13 10:18:20 -0400

    added static fps at 60, and the delta time

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,6 +21,8 @@ def main():
         # Fill the screen with black
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
