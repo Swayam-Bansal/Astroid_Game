de61aca92095e5b9d8c30422ac9dc474c5006d3c
swayam <swayambansal01@gmail.com>
2025-05-13 02:46:05 -0400

    created a constants file, and boiler plate code to open a blank pygame window

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pygame
+from constants import *
 
 def main():
     print("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        
+        # Fill the screen with black
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
