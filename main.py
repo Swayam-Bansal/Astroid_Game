92d83487cf4648548379ebe9de5a1e4b2354bb32
swayam <swayambansal01@gmail.com>
2025-05-13 10:36:50 -0400

    created a basic player model

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
+
 from constants import *
+from player import Player
+from circleshape import CircleShape
 
 def main():
     print("Starting Asteroids!")
@@ -9,6 +12,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
     clock = pygame.time.Clock()
     dt = 0
 
@@ -17,9 +22,14 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-        
+
         # Fill the screen with black
         screen.fill((0, 0, 0))
+
+        # draw the player
+        player.draw(screen)
+
+        # update the whole screen
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
